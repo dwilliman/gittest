607a4dada6ec18178ee1e7d770ca6121ae5bdbc6
Dan <danielwilliman@gmail.com>
2021-05-21 14:46:42 +0100

    added some python files

---
+++ ex3.py
@@ -0,0 +1,24 @@
+print("I will now count my chickens:")
+
+print("Hens", 25.0 + 30.0 / 6.0) # I think this should equal 30
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0) # the answer is 97 since 25*3 is 75
+# cont. the remainder of 75/4 is 3, since you are able to get to 72 by doing 18*4
+
+print("Now I will count the eggs:")
+
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0) #6 -5 -0.25 + 6 , so 0.75 + 6 which equals 6.75
+
+print("Is it true that 3 + 2 < 5 - 7") #yes it is true that 5 is less than -2
+
+print(3.0 + 2.0 < 5.0 - 7.0)
+
+print("What is 3 + 2?", 3.0 + 2.0)
+print("What is 5 - 7?", 5.0 - 7.0)
+
+print("Oh, that's why it is False")
+
+print("How about some more.")
+
+print("Is it greater?", 5.0 > - 2.0)
+print("Is it greater or equal?", 5.0 >= - 2.0)
+print("Is it less or equal", 5.0 <= -2.0)
